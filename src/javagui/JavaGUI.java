41d1690dd310d2ee0f7120be2155e36295a894e4
WanguM <susanwangu@gmail.com>
2018-11-28 12:22:44 +0300

    second commit

---
+++ src/javagui/JavaGUI.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javagui;
+
+import java.awt.Button;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Maggie
+ */
+public class JavaGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    /*    JFrame first = new JFrame("First Frame");
+        first.setSize(500, 500);
+        Button btn = new Button("Click");
+        btn.setSize(50,20);
+        first.add(btn);
+        
+         JPanel p = new JPanel();
+    p.setLayout(null);
+    btn.setBounds(40, 100, 100, 60);
+    p.add(btn);
+
+    first.add(p);
+    // setLayout(null);
+    first.setDefaultCloseOperation(3);
+    first.setSize(400, 400);
+    first.setVisible(true); */
+    
+    NewJFrame second = new NewJFrame();
+   second.setVisible(true);
+       
+    } 
+    
+}
